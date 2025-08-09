e1de1a0c6da25bda7a2df10a590fdfdce02fbdd7
Mouhamadoul amine Faye <mouhamadoulaminefaye@MacBook-Pro-de-Mouhamadoul.local>
2025-08-09 22:16:28 +0000

    changement couleurlogin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -37,6 +37,8 @@
     <link rel="stylesheet" href="{{ asset('assets/vendor/css/core.css') }}" class="template-customizer-core-css" />
     <link rel="stylesheet" href="{{ asset('assets/vendor/css/theme-default.css') }}" class="template-customizer-theme-css" />
     <link rel="stylesheet" href="{{ asset('assets/css/demo.css') }}" />
+    <!-- Custom Colors - Jared Spa Brand Colors -->
+    <link rel="stylesheet" href="{{ asset('assets/css/custom-colors.css') }}" />
 
     <!-- Vendors CSS -->
     <link rel="stylesheet" href="{{ asset('assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
